88036f5279e98ae53871313c3ccdb2a888746bd1
wryyyyyyyy <no@spam.here>
2021-01-05 03:17:39 +0100

    added circleci

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# docker
-for some testing purposes  
-and my docker images
+# docker ![CircleCI](https://circleci.com/gh/wryyyyyyyy/docker.svg?style=shield&circle-token=96870f3cbda563d780f520aa67eb71f915b61f48)
---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo echo '# docker ![CircleCI](https://circleci.com/gh/wryyyyyyyy/docker.svg?style=shield&circle-token=96870f3cbda563d780f520aa67eb71f915b61f48)' >Readme.md
+
+sudo echo '```' >>README.md
+sudo echo "for testing purposes" >>README.md
+sudo echo '```' >>README.md
