0a2911d4d09c5d8be1df0d9fbfdbe90d37d9b422
wryyyyyyyy <no@spam.here>
2021-01-05 03:24:42 +0100

    readme changed

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
 sudo echo '# docker ![CircleCI](https://circleci.com/gh/wryyyyyyyy/docker.svg?style=shield&circle-token=96870f3cbda563d780f520aa67eb71f915b61f48)' >Readme.md
-
-sudo echo '```' >>README.md
 sudo echo "for testing purposes" >>README.md
-sudo echo '```' >>README.md
+
