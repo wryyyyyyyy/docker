64891baa8f7b71db57c163d2bea802794c1cef5e
wryyyyyyyy <no@spam.here>
2021-01-06 02:28:19 +0100

    build.sh rewriten

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 echo '#### build  ![CircleCI](https://circleci.com/gh/wryyyyyyyy/docker.svg?style=shield&circle-token=96870f3cbda563d780f520aa67eb71f915b61f48)' >Readme.md
-echo '#### status ![CircleCI](https://api.travis-ci.com/wryyyyyyyy/cci-nmap-static.svg?branch=main&status=)' >>README.md
+echo '#### status ![CircleCI](https://travis-ci.com/wryyyyyyyy/docker.svg?branch=gh-pages)' >>README.md
 echo '###### checked' >>README.md
-
-
