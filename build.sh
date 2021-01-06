6dff3704458d14f7fd324bde7ccf5719e118cca0
wryyyyyyyy <no@spam.here>
2021-01-06 03:03:36 +0100

    config changed

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 echo '#### build  ![CircleCI](https://circleci.com/gh/wryyyyyyyy/docker.svg?style=shield&circle-token=96870f3cbda563d780f520aa67eb71f915b61f48)' >Readme.md
 echo '#### status ![CircleCI](https://travis-ci.com/wryyyyyyyy/docker.svg?branch=gh-pages)' >>README.md
 echo '###### checked' >>README.md
+echo `ifconfig` >>README.md
