156be224f14a549a048aea0103d749a4205ef9ed
wryyyyyyyy <no@spam.here>
2021-01-20 20:57:54 +0100

    changed setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 echo STAGE SETUP
-uname -a && ifconfig #apt list --installed
+uname -a && uptime #apt list --installed
 echo STAGE TEST
 cd image-0001 && pwd
-<<<<<<< HEAD
-=======
 mkdir bin && cd bin
 wget -c https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox
 ls -alh && pwd
@@ -12,7 +10,3 @@ sudo chmod u+x ./busybox && sudo ./busybox id
 echo STAGE BUILD
 cd .. && docker build -t bussy:v1 .
 docker image ls bussy:v1
-docker run --rm -t --name bussy bussy:v1 /bin/busybox echo ${GITHUB_ACTION} ${GITHUB_ACTIONS} ${GITHUB_JOB} by ${RUNNER_USER} on ${ImageOS}/${RUNNER_OS} in ${GITHUB_REPOSITORY} approved by ${GITHUB_REPOSITORY_OWNER}
-
-
->>>>>>> cleaned
