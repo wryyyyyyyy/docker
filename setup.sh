2f9fde907b6f751d96df5f83b38405507366b43b
wryyyyyyyy <no@spam.here>
2021-01-20 09:36:11 +0100

    changed setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo STAGE SETUP
-#sudo apt-get update
+uname -a && apt list --installed
 echo STAGE TEST
 cd image-0001 && pwd
 <<<<<<< HEAD
@@ -9,6 +9,7 @@ mkdir bin && cd bin
 wget -c https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox
 ls -alh && pwd
 sudo chmod u+x ./busybox && sudo ./busybox id
+echo STAGE BUILD
 cd .. && docker build -t bussy:v1 .
 docker run --rm -t --name bussy bussy:v1 /bin/busybox
 
