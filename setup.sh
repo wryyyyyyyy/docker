a35e30bcb92356d83fdaa370d21d8fa3d2f0ecce
wryyyyyyyy <no@spam.here>
2021-01-20 08:32:02 +0100

    changed setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo STAGE SETUP
-sudo apt-get update
+#sudo apt-get update
+echo STAGE TEST
 cd image-0001 && pwd
 <<<<<<< HEAD
 =======
@@ -8,5 +9,7 @@ mkdir bin && cd bin
 wget -c https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox
 ls -alh && pwd
 sudo chmod u+x ./busybox && sudo ./busybox id
+cd .. && docker build -t bbimg:v1 .
+docker image ls
 
 >>>>>>> cleaned
