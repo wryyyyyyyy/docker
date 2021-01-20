bed532753dc04813d3f47732fcad9ca324c4cd77
wryyyyyyyy <no@spam.here>
2021-01-20 10:29:03 +0100

    changed setup

--- setup.sh
+++ setup.sh
@@ -10,11 +10,8 @@ wget -c https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox
 ls -alh && pwd
 sudo chmod u+x ./busybox && sudo ./busybox id
 echo STAGE BUILD
-export IMAGE=bussy
-export TAG=v1
-cd .. && docker build -t $IMAGE:$tag .
-docker image ls $IMAGE:$TAG
-docker run --rm -t --name bussy $IMAGE:$TAG /bin/busybox echo ${GITHUB_ACTION} ${GITHUB_ACTIONS} ${GITHUB_JOB} by ${RUNNER_USER} on ${ImageOS}/${RUNNER_OS} in ${GITHUB_REPOSITORY} approved by ${GITHUB_REPOSITORY_OWNER}
+cd .. && docker build -t bussy:v1 .
+docker run --rm -t --name bussy bussy:v1 /bin/busybox echo ${GITHUB_ACTION} ${GITHUB_ACTIONS} ${GITHUB_JOB} by ${RUNNER_USER} on ${ImageOS}/${RUNNER_OS} in ${GITHUB_REPOSITORY} approved by ${GITHUB_REPOSITORY_OWNER}
 
 
 >>>>>>> cleaned
