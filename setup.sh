ad2fbf6ad23300d363334dca137b8ac8629e9409
wryyyyyyyy <no@spam.here>
2021-01-20 09:43:45 +0100

    changed setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo STAGE SETUP
-uname -a && apt list --installed
+uname -a && ifconfig #apt list --installed
 echo STAGE TEST
 cd image-0001 && pwd
 mkdir bin && cd bin
@@ -9,5 +9,5 @@ ls -alh && pwd
 sudo chmod u+x ./busybox && sudo ./busybox id
 echo STAGE BUILD
 cd .. && docker build -t bussy:v1 .
-docker run --rm -t --name bussy bussy:v1 /bin/busybox
+docker run --rm -t --name bussy --network="bridge" bussy:v1 /bin/busybox ping -c 4 1.1.1.1
 
