d8d0c0d84aa958ffa0be264b3db3418434eacdc0
wryyyyyyyy <no@spam.here>
2021-01-20 01:08:17 +0100

    cleaned a bit

--- docs/build.sh
+++ docs/build.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-echo '#### build  ![CircleCI](https://circleci.com/gh/wryyyyyyyy/docker.svg)' >Readme.md
-echo '#### status ![TravisCI](https://travis-ci.com/wryyyyyyyy/docker.svg)' >>README.md
-echo '###### checked' >>README.md
+echo STAGE BUILD
