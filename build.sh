283b2d2703e43581644bb0f5d1f5ef727f6c8a33
wryyyyyyyy <no@spam.here>
2021-01-20 23:49:14 +0100

    changed build

--- build.sh
+++ build.sh
@@ -19,13 +19,12 @@ cd .. && ./post.sh
 =======
 #!/bin/bash
 echo STAGE BUILD
-echo ${GITHUB_REPOSITORY}
-sleep 2s
-docker login ghcr.io -p `echo $CONTAINER_TOKEN` -u `echo $GITHUB_REPOSITORY_OWNER`
+docker login ghcr.io -p `echo ${CONTAINER_TOKEN}` -u `echo ${GITHUB_REPOSITORY_OWNER}`
 sleep 2s
 uptime
 mkdir image-0001/bin && cd image-0001/bin
 wget -c -q https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox
+<<<<<<< HEAD
 chmod u+x ./busybox && ./busybox id & cd ..
 <<<<<<< HEAD
 docker build -t bussy:v1 .
@@ -43,5 +42,12 @@ docker image ls ghcr.io/`echo ${GITHUB_REPOSITORY}`/bussy:v1.0.0
 docker run --rm -t --name bussy ghcr.io/`echo ${GITHUB_REPOSITORY}`/bussy:v1.0.0 /bin/busybox
 docker push ghcr.io/`echo ${GITHUB_REPOSITORY}`/bussy:v1.0.0
 >>>>>>> changed setup
+=======
+sudo chmod u+x ./busybox && ./busybox id & cd ..
+docker build . -t ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1.0.0
+docker image ls ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1.0.0
+docker run --rm -t --name bussy ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1.0.0 /bin/busybox
+docker push ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1.0.0
+>>>>>>> changed build
 cd .. && ./post.sh
 >>>>>>> changed build
