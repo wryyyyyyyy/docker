206a56fa8f35777c949c380982dce4db4dbc84dc
wryyyyyyyy <no@spam.here>
2021-01-20 07:52:10 +0100

    tested

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ cd image-0001 && pwd
 mkdir bin && cd bin
 wget -c https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox
 ls -alh && pwd
-#chmod u+x busybox && ./busibox id
+sudo chmod u+x ./busybox && sudo ./busibox id
 
 >>>>>>> cleaned
