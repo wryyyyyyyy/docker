85ed61e07ba1cf45e1ac6e9c4e610ece70efa131
wryyyyyyyy <no@spam.here>
2021-01-20 22:42:32 +0100

    changed setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 echo STAGE SETUP
+<<<<<<< HEAD
 cd image-0001 && pwd
 mkdir bin && cd bin
 wget -c https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox
 ls -alh && pwd
 sudo chmod u+x ./busybox && sudo ./busybox id
+=======
+uname -a && uptime
+>>>>>>> changed setup
 bash build.sh
