299ea81425ec28e2c1856c4b36804251e6337c67
wryyyyyyyy <no@spam.here>
2021-01-21 00:34:56 +0100

    changed build

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 >
 > _For testing purposes only_
 >
+![Develop CI](https://github.com/wryyyyyyyy/docker/workflows/Develop%20CI/badge.svg?branch=develop&event=push)
 
  [![Site](/_site/assets/img/docker.png)](https://wryyyyyyyy.github.com/docker)
--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 echo STAGE SETUP
-id && uname -a && uptime
-bash build.sh
+mkdir image-0001/bin && cd image-0001/bin
+wget -c https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox
+cd .. && sudo chmod u+x bin/busybox && sudo bin/busybox id
+pwd && docker build . -t bussy:v1
+docker login ghcr.io --username `echo ${GITHUB_REPOSITORY_OWNER}` --password `echo ${CONTAINER_TOKEN}`
+docker image ls bussy:v1
+docker run --rm -t --name bussy bussy:v1 /bin/busybox
+docker push ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1
+cd .. && ./post.sh
