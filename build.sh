314c3604497c8492c3500e272ae0ea7b4398228a
wryyyyyyyy <no@spam.here>
2021-01-20 22:19:35 +0100

    changed build

--- build.sh
+++ build.sh
@@ -26,6 +26,11 @@ chmod u+x ./busybox && ./busybox id & cd ..
 docker build -t bussy:v1 .
 docker image ls bussy:v1
 docker run --rm -t --name bussy bussy:v1 /bin/busybox
+<<<<<<< HEAD
 docker build . -t ghcr.io/`echo ${GITHUB_REPOSITORY_OWNER}`/`echo ${GITHUB_REPOSITORY}`/bussy:v1
 bash post.sh
 >>>>>>> changed setup
+=======
+docker build . -t ghcr.io/`echo ${GITHUB_REPOSITORY}`/bussy:v1
+cd .. && ./post.sh
+>>>>>>> changed build
