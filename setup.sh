f329c94e9017da10f582160b7d4f792001fdbaab
wryyyyyyyy <no@spam.here>
2021-01-21 03:44:43 +0100

    changed build

--- setup.sh
+++ setup.sh
@@ -24,6 +24,7 @@ cd .. && sudo chmod u+x bin/busybox && sudo bin/busybox id
 mkdir image-0001/bin
 wget -c https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox -O image-0001/bin
 sudo chmod u+x image-0001/bin/busybox && sudo bin/busybox id
+<<<<<<< HEAD
 >>>>>>> changed build
 docker build . -t ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1.0
 docker login ghcr.io --username `echo ${GITHUB_REPOSITORY_OWNER}` --password `echo ${CONTAINER_TOKEN}`
@@ -31,4 +32,12 @@ docker run --rm -t ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1.0 /bin/busybox
 docker image ls ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1.0
 docker push ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1.0
 cd .. && ./post.sh
+>>>>>>> changed build
+=======
+#docker build . -t ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1.0
+#docker login ghcr.io --username `echo ${GITHUB_REPOSITORY_OWNER}` --password `echo ${CONTAINER_TOKEN}`
+#docker run --rm -t ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1.0 /bin/busybox
+#docker image ls ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1.0
+#docker push ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1.0
+#cd .. && ./post.sh
 >>>>>>> changed build
