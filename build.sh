06e5bc77b6f6b5afb26ecdbdd17305c6b8f9bbca
wryyyyyyyy <no@spam.here>
2021-01-21 02:09:21 +0100

    changed build

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ echo STAGE BUILD
 mkdir image-0001/bin && cd image-0001/bin
 wget -c -q https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox
 sudo chmod u+x ./busybox && sudo ./busybox id & cd ..
-`echo ${CONTAINER_TOKEN}` | docker login ghcr.io --username `echo ${GITHUB_REPOSITORY_OWNER}` --password-stdin
+#`echo ${CONTAINER_TOKEN}` | docker login ghcr.io --username `echo ${GITHUB_REPOSITORY_OWNER}` --password-stdin
 sleep 2
-docker build . -t bussy:v1
-docker run --rm -t --name bussy bussy:v1 /bin/busybox
-docker image ls bussy:v1
-docker push ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1
+docker build . -t ghcr.io/wryyyyyyyy/bussy:${{ github.sha }}
+#docker run --rm -t --name bussy bussy:${{ github.sha }} /bin/busybox
+docker image ls ghcr.io/wryyyyyyyy/bussy:${{ github.sha }}
+docker push ghcr.io/wryyyyyyyy/bussy:${{ github.sha }}
 cd .. && ./post.sh
