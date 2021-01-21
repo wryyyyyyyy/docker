4140fdb243c3bbc887637c8cb498894b3b89dfb8
wryyyyyyyy <no@spam.here>
2021-01-21 03:35:09 +0100

    changed build

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ echo STAGE SETUP
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 cd image-0001 && pwd
 mkdir bin && cd bin
 wget -c https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox
@@ -19,6 +20,11 @@ bash build.sh
 mkdir image-0001/bin && cd image-0001/bin
 wget -c https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox
 cd .. && sudo chmod u+x bin/busybox && sudo bin/busybox id
+=======
+mkdir image-0001/bin
+wget -c https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox -O image-0001/bin
+sudo chmod u+x image-0001/bin/busybox && sudo bin/busybox id
+>>>>>>> changed build
 docker build . -t ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1.0
 docker login ghcr.io --username `echo ${GITHUB_REPOSITORY_OWNER}` --password `echo ${CONTAINER_TOKEN}`
 docker run --rm -t ghcr.io/${GITHUB_REPOSITORY_OWNER}/bussy:v1.0 /bin/busybox
