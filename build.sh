036092c6689cdeda09d872995a6cbf3f20e94443
wryyyyyyyy <no@spam.here>
2021-01-21 02:49:44 +0100

    changed build

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ wget -c -q https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox
 sudo chmod u+x ./busybox && sudo ./busybox id & cd ..
 #`echo ${CONTAINER_TOKEN}` | docker login ghcr.io --username `echo ${GITHUB_REPOSITORY_OWNER}` --password-stdin
 sleep 2
-docker build . -t ghcr.io/wryyyyyyyy/bussy:v1
+docker build . -t ghcr.io/wryyyyyyyy/scratch:1.0
 #docker run --rm -t --name bussy bussy:${{ github.sha }} /bin/busybox
-docker image ls ghcr.io/wryyyyyyyy/bussy:v1
-docker push ghcr.io/wryyyyyyyy/bussy:v1
-cd .. && ./post.sh
+docker image ls ghcr.io/wryyyyyyyy/scratch:1.0
+docker push ghcr.io/wryyyyyyyy/scratch:1.0
+#cd .. && ./post.sh
